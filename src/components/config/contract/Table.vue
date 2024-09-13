73724018b8ac8f8d8b4a27711320e28e8c8aaa12
BrunoGaffo <67167417+BrunoGaffo@users.noreply.github.com>
2024-09-13 14:14:31 -0300

    up

---
+++ src/components/config/contract/Table.vue
@@ -0,0 +1,40 @@
+<template>
+  <DefaultTable :configs="configs" />
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+
+export default {
+  components: {},
+
+  computed: {
+    ...mapGetters({
+      defaultCompany: "people/defaultCompany",
+    }), 
+    configs() {
+      return {
+        externalFilters: false,
+        store: "contracts",
+        categories: ["contract"],
+        add: true,
+        delete: true,
+        selection: false,
+        search: false,
+        columns: {
+          category: {
+            filters: {
+              context: "contract",
+              company: "/people/" + this.defaultCompany.id,
+            },
+          },
+        },
+      };
+    },
+     
+  },
+};
+</script>
+
+
---
+++ src/pages/Contracts/Contracts.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <Menu />
+
+</template>
+
+<script>
+import Menu from "../../components/config/Menu/Table";
+
+export default {
+  //
+  components: {
+    Menu,
+  },
+
+  computed: {
+
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {},
+};
+</script>
