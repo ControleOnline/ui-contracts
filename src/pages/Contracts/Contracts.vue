7c2e0f1c4cd187485717f423c041a7d3bc637917
BrunoGaffo <67167417+BrunoGaffo@users.noreply.github.com>
2024-09-13 14:31:56 -0300

    up

---
+++ src/pages/Contracts/Contracts.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <Menu />
+
+</template>
+
+<script>
+import Menu from "../../components/config/contract/Table";
+
+export default {
+  //
+  components: {
+    Menu,
+  },
+
+  computed: {
+
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {},
+};
+</script>
