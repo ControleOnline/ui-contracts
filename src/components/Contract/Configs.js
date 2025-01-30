47e87a00d584ffcef3015e1be65bbf866e217987
Luiz Kim <luiz.kim@controleonline.com>
2025-01-30 13:40:29 -0300

    Update

--- src/components/Contract/Configs.js
+++ src/components/Contract/Configs.js
@@ -1,4 +1,3 @@
-import DefaultButtonDialog from "@controleonline/ui-default/src/components/Default/DefaultButtonDialog";
 import getModelConfigs from "@controleonline/ui-crm/src/components/Model/Configs";
 
 export default function getConfigs(context, myCompany, $components, $store) {
@@ -60,7 +59,7 @@ export default function getConfigs(context, myCompany, $components, $store) {
     components: {
       headerActions: [
         {
-          component: DefaultButtonDialog,
+          component: this.$components.DefaultButtonDialog,
           props: {
             component: $components.DefaultTable,
           },
