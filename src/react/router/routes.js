6852816326a5604c1457e8f133f2b149e5a2a692
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 15:04:03 -0300

    Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,17 +1,9 @@
 import Contract from '@controleonline/ui-contracts/src/react/pages/ContractDetails';
-import CrmLayout from '@controleonline/ui-layout/src/react/layouts/CrmLayout';
 
-import React from 'react';
-
-const WrappedClient = ({navigation, route}) => (
-  <CrmLayout navigation={navigation} route={route}>
-    <Contract navigation={navigation} route={route} />
-  </CrmLayout>
-);
 const contractRoutes = [
   {
     name: 'ContractDetails',
-    component: WrappedClient,
+    component: Contract,
     options: {
       headerShown: false,
       title: 'Contrato',
