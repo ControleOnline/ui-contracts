22342bd306e03cecd7cf98c7a21795c19938ab64
Alexandre de Silva Cunha <alemac@mac.com>
2026-03-02 17:33:18 -0300

    removido SafeAreaView

--- src/react/components/contracts.js
+++ src/react/components/contracts.js
@@ -4,10 +4,10 @@ import {
   View,
   TouchableOpacity,
   ScrollView,
-  SafeAreaView,
   ActivityIndicator,
   StyleSheet,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import {useNavigation, useFocusEffect} from '@react-navigation/native';
 import {useStores} from '@store';
 import Icon from 'react-native-vector-icons/MaterialIcons';
